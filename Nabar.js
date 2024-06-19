3181c3f6571b10fb68ecfba3acff693694fcb053
Ramakrishna15 <98575273+Ramakrishna15@users.noreply.github.com>
2024-06-19 14:01:31 +0530

    Add files via upload

---
+++ Nabar.js
@@ -0,0 +1,20 @@
+// src/Navbar.js
+import React from 'react';
+import './Navbar.css';
+
+// Rest of the code remains the same
+
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
